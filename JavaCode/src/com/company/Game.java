cd8e9acece589c9ac07e76f0f5a40cad337df0a2
Lokoselli <51972451+Lokoselli@users.noreply.github.com>
2020-07-21 16:00:56 -0300

    Create Class Game & Player. Create gamelog

---
+++ JavaCode/src/com/company/Game.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+public class Game {
+
+    private int totalKills;
+    private Hashtable<Integer, Player> playersHashTable;
+    private ArrayList<Player> playerList;
+
+}
---
+++ JavaCode/src/com/company/Player.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Player {
+
+    private int kills;
+    private String name;
+    private int id;
+
+    public int getKills() {
+        return kills;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
